a59c430a68c307bf9ef79aaa6bae24055587fcb0
Daniel Paz <daniel@paztoya.com>
2020-01-20 04:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function changeCompletely(elements, index, array)
 {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + `${array[index]}s!!!`;
-  
+
 }
 var animals = [ "dog", "cat", "squirrel"];
 
